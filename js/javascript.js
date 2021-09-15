d1736710d0ffd2f5da383a667fae2cd8f3b4257f
BjoergWR <78902994+BjoergWR@users.noreply.github.com>
2021-09-15 12:07:43 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -11,7 +11,7 @@ function widthHeight(){
     //getting the viewport dimensions
     let width= Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     let height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    alert("fun widthHeight Your screen resolution is: " + width + "x" + height);
+    //alert("fun widthHeight Your screen resolution is: " + width + "x" + height);
 }
 
 

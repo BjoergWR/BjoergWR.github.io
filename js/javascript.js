ccc3d5de9c16d61fdfa8c5a9e8efbaafe6a9f8a0
BjoergWReinert <bjorg.rei20@student.setur.fo>
2021-09-15 12:02:18 +0100

    js

--- js/javascript.js
+++ js/javascript.js
@@ -6,8 +6,18 @@ function getResolution() {
 }
 
 
+
+function widthHeight(){
+    //getting the viewport dimensions
+    let width= Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    let height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    alert("fun widthHeight Your screen resolution is: " + width + "x" + height);
+}
+
+
 function start(){
-    getResolution();
+    //getResolution();
+    widthHeight();
 }
 
 window.addEventListener("load", start);

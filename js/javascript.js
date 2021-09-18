ad564b9ee99e566ad55a682e414467c38b17a701
BjoergWReinert <bjorg.rei20@student.setur.fo>
2021-09-18 21:11:32 +0100

    valitate input js fun.

--- js/javascript.js
+++ js/javascript.js
@@ -14,34 +14,44 @@ function addAndDisplaySum(price){
 
 }
 
-
+function validateForm(a, b, c, d, f) {
+    if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "", f == null || f == "") {
+        alert("Please Fill All Required Field");
+        return false;
+    }
+    else
+        return true;
+}
 
 
 
 function confirmFrom() {
-    let person = {name:"", email:"", phone:"", city:"", street:"", comments:""};
+    let person = {name: "", email: "", phone: "", city: "", street: "", comments: ""};
     person.name = document.querySelector('#navn').value;
     person.email = document.querySelector('#email').value;
     person.phone = document.querySelector('#phone').value;
     person.city = document.querySelector('#city').value;
     person.street = document.querySelector('#street').value;
     person.comments = document.querySelector('#comments').value;
-    var txt;
-    var confirmText = "Hi " + person.name + ". \nPlease confirm your buy.";
-    var replay = confirm(confirmText);
-    if (replay) {
-        let elem = document.getElementById('form_registration');
-        elem.parentNode.removeChild(elem);
+    var valdiInput = validateForm(person.name, person.email, person.phone, person.city, person.street);
+    if (valdiInput) {
+        var txt;
+        var confirmText = "Hi " + person.name + ". \nPlease confirm your buy.";
+        var replay = confirm(confirmText);
+        if (replay) {
+            let elem = document.getElementById('form_registration');
+            elem.parentNode.removeChild(elem);
 
-        txt = "Order confirmation.<br> Order number: 48557. <br>" + "Name: " + person.name + "<br>Address: "
-            + person.street + ", " + person.city + "<br>The order is ready in 40 min.";
-        document.getElementById('insert_submittet').innerHTML = txt;
+            txt = "Order confirmation.<br> Order number: 48557. <br>" + "Name: " + person.name + "<br>Address: "
+                + person.street + ", " + person.city + "<br>The order is ready in 40 min.";
+            document.getElementById('insert_submittet').innerHTML = txt;
 
-    } else {
-        txt = "Hi " + person.name + "you hav cancel your order";
+        } else {
+            txt = "Hi " + person.name + "you hav cancel your order";
+        }
     }
+}
 
-    }
 
 
 

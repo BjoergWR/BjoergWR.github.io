38977e292febae16cbcd43f13a596a7e72cbcac3
BjoergWReinert <bjorg.rei20@student.setur.fo>
2021-09-18 20:08:38 +0100

    person navn á pupup

--- js/javascript.js
+++ js/javascript.js
@@ -31,19 +31,14 @@ function confirmFrom() {
     var confirmText = "Hi " + person.name + ". Please confirm your buy"
     var replay = confirm(confirmText);
     if (r) {
-        txt = "Order confirmation." + "<br/>" +"Order number: 48557. " + "<br/>" + "Name: " + person.name + "<br/>"
-        + "Address: " + person.street + ", " + person.city"<br/>" + "The order is ready in 40 min.";
+        txt = "Order confirmation. Order number: 48557." + "Name: " + person.name + "Address: "
+            + person.street + ", " + person.city + "The order is ready in 40 min.";
+    alert(txt);
 
     } else {
         txt = "Hi " + person.name + "you hav cancel your order";
     }
-    let parentNode = document.getElementById("main_content");
-    console.log(parentNode);
-    let oldNode = document.getElementsByClassName("kontakt");
-    console.log(oldNode);
-    let newNode = document.createTextNode(txt);
-    console.log(newNode);
-    //parentNode.replaceChild(newNode, oldNode);
+
     }
 
 

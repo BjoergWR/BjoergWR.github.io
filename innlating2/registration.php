f879608beb724ea7a30a7d0f00ff4f50a8769f07
BjoergWReinert <bjorg.rei20@student.setur.fo>
2021-10-08 18:38:39 +0100

    "register and style"

--- innlating2/registration.php
+++ innlating2/registration.php
@@ -4,39 +4,34 @@ include('view/header.php');
 ?>
 <section class="bg-dark prime--container last--container">
     <div class="container from_top">
-        <div><p id="insert_submittet"></p></div>
-        <div class="kontakt" id="form_registration">
+        <div><p id="insert_submittet"><h1>Checkout form</h1></p></div>
             <form id="registrationForm">
-                <p><label for="navn">Navn: </label>
-                    <input type="text" name="name" id="navn" placeholder="Liam Terjson" required/></p>
+                <p><label for="navn">Name: </label>
+                    <input type="text" name="name" id="navn" placeholder="" required/></p>
 
                 <p><label for="email">Email: </label>
-                    <input type="text" name="email" id="email" placeholder="burgar@stova.com" required/></p>
+                    <input type="text" name="email" id="email" placeholder="you@example.com" required/></p>
 
-                <p><label for="phone">Nummar: </label>
+                <p><label for="phone">Phone: </label>
                     <input type="tel" name="telefon" id="phone" placeholder= "505050" pattern = "\d{6}" required/></p>
 
-                <p><label for="city">Býur: </label>
-                    <input type="text" name="býur" id="city" placeholder="Tórshavn" required/></p>
+                <p><label for="city">City: </label>
+                    <input type="text" name="city" id="city" placeholder="Tórshavn" required/></p>
 
-                <p><label for="street">Gøta: </label>
-                    <input type="text" name="gøta" id="street" placeholder="Tórsgøta 100" required/></p>
+                <p><label for="street">Street: </label>
+                    <input type="text" name="street" id="street" placeholder="Tórsgøta 100" required/></p>
 
-                <p><label for="comments">Viðmerk:</label>
-                    <textarea name="comments" id="comments" placeholder=" Viðmeringar" rows="3" cols="25"></textarea></p>
+                <p><label for="comments">Comment:</label>
+                    <textarea name="comments" id="comments" placeholder="Write a comment" rows="3" cols="25"></textarea></p>
 
 
                 <p class="submit">
-                    <input type="button" value="send" name="submit" class="button1" onclick="confirmFrom();" />
-                    <input type="reset" name="reset" class="button2" value="Strika"/></p>
+                    <input type="reset" type='submit' name="reset" class='action_btn remove_button' value="reset"/>
+                    <input type="button" type='submit'name="confirm" class='action_btn add_button'  value="confirm"/>
+                    </p>
             </form>
-        </div>
     </div>
 </section>
-
-
-
-
 
 
 

d1f92d5e83bde08af4dab43425a988ac4c15b448
BjoergWReinert <bjorg.rei20@student.setur.fo>
2021-10-09 14:03:20 +0100

    "register"

--- innlating2/registration.php
+++ innlating2/registration.php
@@ -2,27 +2,28 @@
 $page = "registration";
 include('view/header.php');
 ?>
+
 <section class="bg-dark prime--container last--container">
     <div class="container from_top">
         <div><p id="insert_submittet"><h1>Checkout form</h1></p></div>
-            <form id="registrationForm">
-                <p><label for="navn">Name: </label>
-                    <input type="text" name="name" id="navn" placeholder="" required/></p>
+            <form action="registration.php"  method="post">
+                <p><label for="fnavn">Name: </label>
+                    <input type="text" name="fname" id="name" placeholder="" required/></p>
 
-                <p><label for="email">Email: </label>
-                    <input type="text" name="email" id="email" placeholder="you@example.com" required/></p>
+                <p><label for="femail">Email: </label>
+                    <input type="text" name="femail" id="email" placeholder="you@example.com" required/></p>
 
-                <p><label for="phone">Phone: </label>
-                    <input type="tel" name="telefon" id="phone" placeholder= "505050" pattern = "\d{6}" required/></p>
+                <p><label for="fphone">Phone: </label>
+                    <input type="tel" name="fphone" id="phone" placeholder= "505050" pattern = "\d{6}" required/></p>
 
-                <p><label for="city">City: </label>
-                    <input type="text" name="city" id="city" placeholder="Tórshavn" required/></p>
+                <p><label for="fcity">City: </label>
+                    <input type="text" name="fcity" id="city" placeholder="Tórshavn" required/></p>
 
-                <p><label for="street">Street: </label>
-                    <input type="text" name="street" id="street" placeholder="Tórsgøta 100" required/></p>
+                <p><label for="fstreet">Street: </label>
+                    <input type="text" name="fstreet" id="street" placeholder="Tórsgøta 100" required/></p>
 
-                <p><label for="comments">Comment:</label>
-                    <textarea name="comments" id="comments" placeholder="Write a comment" rows="3" cols="25"></textarea></p>
+                <p><label for="fcomments">Comment:</label>
+                    <textarea name="fcomments" id="comments" placeholder="Write a comment" rows="3" cols="25"></textarea></p>
 
 
                 <p class="submit">
@@ -32,8 +33,6 @@ include('view/header.php');
             </form>
     </div>
 </section>
-
-
 
 
 

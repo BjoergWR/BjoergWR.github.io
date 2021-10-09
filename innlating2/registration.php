3e6606ee6e30ab7e0a911bba7730ace128b10057
BjoergWReinert <bjorg.rei20@student.setur.fo>
2021-10-09 14:34:53 +0100

    "arb uppá style"

--- innlating2/registration.php
+++ innlating2/registration.php
@@ -4,32 +4,32 @@ include('view/header.php');
 ?>
 
 <section class="bg-dark prime--container last--container">
-    <div class="container from_top">
-        <div><p id="insert_submittet"><h1>Checkout form</h1></p></div>
+    <div class="container from_top border">
+        <div><h1>Checkout form</h1></div>
             <form action="registration.php"  method="post">
-                <p><label for="fnavn">Name: </label>
-                    <input type="text" name="fname" id="name" placeholder="" required/></p>
+                <label class="lable" for="fnavn">Name: </label>
+                <input class="input" type="text" name="fname" id="name" placeholder="" required/>
 
-                <p><label for="femail">Email: </label>
-                    <input type="text" name="femail" id="email" placeholder="you@example.com" required/></p>
+                <label class="lable" for="femail">Email: </label>
+                <input class="input" type="text" name="femail" id="email" placeholder="you@example.com" required/>
 
-                <p><label for="fphone">Phone: </label>
-                    <input type="tel" name="fphone" id="phone" placeholder= "505050" pattern = "\d{6}" required/></p>
+                <label class="lable" for="fphone">Phone: </label>
+                <input class="input" type="tel" name="fphone" id="phone" placeholder= "505050" pattern = "\d{6}" required/>
 
-                <p><label for="fcity">City: </label>
-                    <input type="text" name="fcity" id="city" placeholder="Tórshavn" required/></p>
+                <label class="lable" for="fcity">City: </label>
+                <input class="inpute" type="text" name="fcity" id="city" placeholder="Tórshavn" required/>
 
-                <p><label for="fstreet">Street: </label>
-                    <input type="text" name="fstreet" id="street" placeholder="Tórsgøta 100" required/></p>
+                <label class="lable" for="fstreet">Street: </label>
+                <input class="input" type="text" name="fstreet" id="street" placeholder="Tórsgøta 100" required/>
 
-                <p><label for="fcomments">Comment:</label>
-                    <textarea name="fcomments" id="comments" placeholder="Write a comment" rows="3" cols="25"></textarea></p>
+                <label class="lable" for="fcomments">Comment:</label>
+                <textarea class="input" name="fcomments" id="comments" placeholder="Write a comment" rows="3" cols="25"></textarea>
 
 
-                <p class="submit">
+                <div class="submit">
                     <input type="reset" type='submit' name="reset" class='action_btn remove_button' value="reset"/>
                     <input type="button" type='submit'name="confirm" class='action_btn add_button'  value="confirm"/>
-                    </p>
+                    </div>
             </form>
     </div>
 </section>
